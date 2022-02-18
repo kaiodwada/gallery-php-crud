62b1fd6d8d6f8c9bdd12c9768d56b606cfbe5073
Kaio Alves <49643563+kaiodwada@users.noreply.github.com>
2022-02-18 13:51:44 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,22 @@
+ErroNome = document.getElementById("ErroNome");
+ErroImg = document.getElementById("ErroImg");
 
-$('#formImg').submit(function (e) {
-    e.preventDefault();
-    let nome = $('#nome').val();
-    let img = $('#img').val();
-    let formdata = new FormData();
-    formdata.append('imagem', img);
-    formdata.append('name', nome);
-
+function validar() {
+  let nome = formImg.nome.value;
+  let img = formImg.img.value;
+  
     if (nome == "") {
+        event.preventDefault();
         ErroNome.style.display = "block";
         formImg.nome.focus();
         return false;
     }
     if (img == "") {
+        event.preventDefault();
         ErroImg.style.display = "block";
         formImg.img.focus();
         return false;
     }
-    $.ajax({
-        url: 'model/crud/create.php',
-        method: 'POST',
-        data: formdata,
-        dataType: 'json',
-        enctype: "multpart/form-data",
-        processData: false,
-        contentType: false,
-        beforeSend: function () {
-            $('.loader').css({ display: "block" });
-        },
-        complete: function () {
-            $('.loader').css({ display: "none" });
-        }
-    }).done(function (result) {
-        $('#nome').val('');
-        $('#img').val('');
-        console.log(result);
-    });
-});
+};
+
+
