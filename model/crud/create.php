e5499268fa96d9cb1c3bfe0fba638b70d1c3a8bd
Kaio Alves <49643563+kaiodwada@users.noreply.github.com>
2022-02-18 13:47:43 -0300

    Update model

--- model/crud/create.php
+++ model/crud/create.php
@@ -3,21 +3,17 @@ require_once("../servidor/conexao.php");
 $nome_img = $_POST['nome'];
 $img = $_FILES['img'];
 $dir = "uploads/";
-$extensao = strtolower
-(substr($_FILES['img']['name'], -4));
-$novo_nome = uniqid().$extensao;
-move_uploaded_file
-($_FILES['img']['tmp_name'], $dir.$novo_nome);
+$extensao = strtolower(substr($_FILES['img']['name'], -4));
+$novo_nome = uniqid() . $extensao;
+move_uploaded_file($_FILES['img']['tmp_name'], $dir . $novo_nome);
 if (isset($nome_img) || isset($img)) {
     $sql = "INSERT INTO dados(nome_img, nome) VALUES('$nome_img', '$novo_nome')";
     if (mysqli_query($conn_bd, $sql)) {
-echo "<script>document.location.href = '../../templates/galeria.php';
-</script>";
-echo "Imagem adicionada";
-    } else {
-        echo "Erro: " . $sql . "<br>" . mysqli_error($conn_bd);
+        echo "<script>
+document.location.href = '../../templates/galeria.php';
+</script>
+        ";
     }
 } else {
     echo "Preencha os campos";
 }
-?>
