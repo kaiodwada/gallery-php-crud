90275bdebd1443155b9ba04194c699034094d313
kaiodwada <kaio-giovany@hotmail.com>
2022-04-20 21:38:11 -0300

    Removendo arquivos

--- model/crud/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("../servidor/conexao.php");
-$id_img = $_GET["id"];
-$sql = "DELETE FROM dados WHERE id = $id_img";
-$deletar = $conn_bd->query($sql) or die($conn_bd->error);
-echo "<script>location.href='../../templates/galeria.php'</script>";
-$conm_bd->close();
-?>
--- model/crud/update.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("../servidor/conexao.php");
-$id_img = $_GET["id"];
-$nome = $_GET["nome_img"];
-$sql = "UPDATE dados SET nome_img ='$nome' WHERE id = $id_img";
-$update = $conn_bd->query($sql);
-echo "<script>location.href='../../templates/galeria.php'</script>";
-?>
